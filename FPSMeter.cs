9848ce77b952de30f890b8c1b6ee8f107cd4cbf9
Bogdan Evsenev <evsenev.bogdan@gmail.com>
2015-01-29 19:52:39 +0500

    FPS Meter for Unity3D 4.6+ (new UI).

---
+++ FPSMeter.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+// This script working with Text GameObject in new UI of Unity3D ( >= 4.6)
+public class FPSMeter : MonoBehaviour
+{
+	private float updateInterval = 0.5f;
+	private int frames = 0;
+	private float accum = 0f;
+	private float timeleft = 0.0f;
+
+	private Text txtFPS; //Text Object
+
+	private void Start()
+	{
+		timeleft = updateInterval;
+		txtFPS = GetComponent<Text>(); //GetTextObject
+	}
+
+	private void Update()
+	{
+		timeleft -= Time.deltaTime;
+		accum += Time.timeScale/Time.deltaTime;
+		++frames;
+
+		if (timeleft <= 0f)
+		{
+			string fpsText ="FPS: " + (accum/frames).ToString("f2");
+			txtFPS.text = fpsText;
+			timeleft = updateInterval;
+			accum = 0f;
+			frames = 0;
+		}
+	}
+}
